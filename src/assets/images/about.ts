9d4752602987809ca2812e9f04ab7b419b298f29
Yaroslav <6715414@gmail.com>
2023-02-23 21:13:42 +0200

    added links for images

---
+++ src/assets/images/about.ts
@@ -0,0 +1,11 @@
+import img1 from "./1.webp";
+import img2 from "./2.webp";
+import img3 from "./3.webp";
+
+export const speedImgs = [0, 5, -2];
+export const aboutImgs = [img2, img1, img3];
+export const linkImgs = [
+  "https://github.com/zazazaza4/Flappy_Bird_JS_Neuroevolution",
+  "https://github.com/zazazaza4/Full_Stack_Blog_Client",
+  "https://github.com/zazazaza4/Car_Website",
+];
