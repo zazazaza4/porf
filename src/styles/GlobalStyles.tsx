745108260b5f2580c956a07b45cd9d645cf39926
Yaroslav <6715414@gmail.com>
2023-02-23 17:36:57 +0200

    added styles

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components";
+
+import "@fontsource/kaushan-script";
+import "@fontsource/sirin-stencil";
+
+const GlobalStyles = createGlobalStyle`
+    html.has-scroll-smooth {
+        overflow: hidden;
+        position: fixed;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;  
+    }
+
+    *,*::before,*::after {
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        font-family: "Sirin Stencil";
+        overflow-x: hidden;
+    }
+
+    h1,h2,h3,h4,h5,h6 {
+        margin: 0;
+        padding: 0;
+    }
+
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+`;
+
+export default GlobalStyles;
