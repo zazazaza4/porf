502d2487bb2e1e85ac10e3ed8a1ddb47e780a9f9
Yaroslav <6715414@gmail.com>
2023-02-24 11:16:24 +0200

    added exports for components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { MainTitle } from "./MainTitle";
+export { Loader } from "./Loader";
+export { ScrollTriggerProxy } from "./ScrollTriggerProxy";
+export { NavBar } from "./NavBar";
+export { Logo } from "./Logo";
+export { CoverVideo } from "./CoverVideo";
