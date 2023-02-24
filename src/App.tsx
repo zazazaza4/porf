f95a31600c2c1f7c92b8e353dd56390243c50950
Yaroslav <6715414@gmail.com>
2023-02-24 11:17:09 +0200

    added settings for loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 import { ThemeProvider } from "styled-components";
-import { Home } from "./sections";
+import { About, Footer, Home } from "./sections";
 
 import GlobalStyles from "./styles/GlobalStyles";
 import { dark } from "./styles/Themes";
 import "locomotive-scroll/dist/locomotive-scroll.css";
 import { AnimatePresence } from "framer-motion";
+import { Loader, ScrollTriggerProxy } from "./components";
 
 function App() {
   const containerRef = useRef<HTMLElement | null>(null);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 3000);
+  }, []);
 
   return (
     <>
@@ -29,9 +37,14 @@ function App() {
           watch={[]}
           containerRef={containerRef}
         >
+          <AnimatePresence>{isLoaded ? null : <Loader />}</AnimatePresence>
           <AnimatePresence>
             <main className="App" ref={containerRef} data-scroll-container>
+              <ScrollTriggerProxy />
+
               <Home />
+              <About />
+              <Footer />
             </main>
           </AnimatePresence>
         </LocomotiveScrollProvider>
