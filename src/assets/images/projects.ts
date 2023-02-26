910484246445d1a7689c529a3ba4836a00d32bed
Yaroslav <6715414@gmail.com>
2023-02-26 23:25:32 +0200

    added the assets for Project section

---
+++ src/assets/images/projects.ts
@@ -0,0 +1,51 @@
+import img1 from "./1.webp";
+import img2 from "./2.webp";
+import img3 from "./3.webp";
+import img4 from "./4.webp";
+import img5 from "./5.webp";
+import img6 from "./6.webp";
+import img7 from "./7.webp";
+import img8 from "./8.webp";
+
+export const productArray = [
+  {
+    img: img1,
+    title: "Full Stack Blog",
+    link: "https://github.com/zazazaza4/Full_Stack_Blog_Client",
+  },
+  {
+    img: img2,
+    title: "Flappy Bird (TensorFlow.js)",
+    link: "https://github.com/zazazaza4/Flappy_Bird_JS_Neuroevolution",
+  },
+  {
+    img: img3,
+    title: "Car App",
+    link: "https://github.com/zazazaza4/Car_Website",
+  },
+  {
+    img: img4,
+    title: "Chat App",
+    link: "https://github.com/zazazaza4/Chat_App",
+  },
+  {
+    img: img5,
+    title: "Movie App",
+    link: "https://github.com/zazazaza4/Movie_React_Project",
+  },
+  {
+    img: img6,
+    title: "Sorting Algorithms Animations",
+    link: "https://github.com/zazazaza4/Sorting_Algorithms_JS",
+  },
+  {
+    img: img7,
+    title: "Snake game",
+    link: "https://github.com/zazazaza4/Snake_JS_Nure",
+  },
+  {
+    img: img8,
+    title: "Currency converter",
+    link: "https://github.com/zazazaza4/Currency_Converter",
+  },
+];
